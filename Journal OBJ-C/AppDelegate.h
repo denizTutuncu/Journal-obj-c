b7bbe0a5c502ca8c25147aa327f15cc31c7fbe81
Deniz Tutucu <denizsafaktutuncu@gmail.com>
2019-02-11 13:21:39 -0700

    Initial Commit

---
+++ Journal OBJ-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Journal OBJ-C
+//
+//  Created by Deniz Tutuncu on 2/11/19.
+//  Copyright © 2019 Deniz Tutuncu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
